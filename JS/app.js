a3956ad51feca0047f8b404b1dea3a60fa5ef69b
Martin Picasarri <martinpicasarri@gmail.com>
2024-11-29 12:20:24 -0300

    Version 12

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,4 @@
-function InicioUsuario() {
+/*function InicioUsuario() {
     let usuario, apellido, ubicacion, telefono;
     let telefonoValido = false;
   
@@ -17,49 +17,55 @@ function InicioUsuario() {
     alert(`Su usuario es ${usuario} ${apellido} y vive en ${ubicacion}. Su teléfono es ${telefono}`);
   }
   
-  InicioUsuario();
+  InicioUsuario();*/
 
 class Hamburguesa {
-    constructor (nombre="", descripcion="", precio=0){
+    constructor (nombre="", descripcion="", precio=0, boton="") {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
+        this.boton = boton;
     }
 }
-const HamburguesaClasica = new Hamburguesa ("Clasica","Contiene pan de papa, medallon de carne y queso cheddar x2", 9800 );
-const HamburguesaVegetariana = new Hamburguesa ("Vegetariana","Contiene pan de papa, medallon de verduras y queso cheddar x2", 9500 );
-const HamburguesaBacon = new Hamburguesa ("Bacon","Contiene pan de papa, medallon de carne con panceta x2 y queso cheddar x2", 11000 );
-const HamburguesaConPollo = new Hamburguesa ("Pollo","Contiene pan de papa, medallon de pollo x2 y queso cheddar x2", 10000 );
-const HamburguesaCheese = new Hamburguesa ("Cheese","Contiene pan de papa, medallon de carne x2 con queso cheddar x2", 12000 );
-const HamburguesaChili = new Hamburguesa ("Chili","Contiene pan de papa, medallon de carne x2, salsa picante y queso cheddar x2", 12500 );
-const HamburguesaBlueCheese = new Hamburguesa ("Blue Cheese","Contiene pan de papa, medallon de carne x2 y queso azul x2", 13000 );
+const HamburguesaClasica = new Hamburguesa ("Clasica","Contiene pan de papa, medallon de carne y queso cheddar x2. Acompañada con papas fritas.", 9800, "CLASICA");
+const HamburguesaVegetariana = new Hamburguesa ("Vegetariana","Contiene pan de papa, medallon de verduras y queso cheddar x2. Acompañada con papas fritas.", 9500, "VEGETARIANA");
+const HamburguesaBacon = new Hamburguesa ("Bacon","Contiene pan de papa, medallon de carne con panceta x2 y queso cheddar x2. Acompañada con papas fritas.", 11000, "BACON");
+const HamburguesaConPollo = new Hamburguesa ("Pollo","Contiene pan de papa, medallon de pollo x2 y queso cheddar x2. Acompañada con papas fritas.", 10000, "POLLO");
+const HamburguesaCheese = new Hamburguesa ("Cheese","Contiene pan de papa, medallon de carne x2 con queso cheddar x2. Acompañada con papas fritas.", 12000, "CHEESE");
+const HamburguesaChili = new Hamburguesa ("Chili","Contiene pan de papa, medallon de carne x2, salsa picante y queso cheddar x2, acompanada con papas fritas.", 12500, "CHILI");
+const HamburguesaBlueCheese = new Hamburguesa ("Blue Cheese","Contiene pan de papa, medallon de carne x2 y queso azul x2. Acompañada con papas fritas", 13000, "BLUECHEESE");
+const hamburguesaBBQ = new Hamburguesa ("BBQ","Contiene pan de papa, medallon de carne x2, salsa BBQ y queso cheddar x2. Acompañada con papas fritas.", 11500, "BBQ");
+const HamburguesaBigBurguer= new Hamburguesa ("Big Burguer","Contiene pan de papa, medallon de carne x3, cebolla caramelizada, bacon y queso cheddar x3. Acompañada con papas fritas.", 13000, "BIG-BURGUER");
 
-const menuHamburguesa = [HamburguesaClasica, HamburguesaVegetariana, HamburguesaBacon, HamburguesaConPollo, HamburguesaCheese, HamburguesaChili, HamburguesaBlueCheese];
+const menuHamburguesa = [HamburguesaClasica, HamburguesaVegetariana, HamburguesaBacon, HamburguesaConPollo, HamburguesaCheese, HamburguesaChili, HamburguesaBlueCheese, hamburguesaBBQ, HamburguesaBigBurguer];
+
 
 function crearMenu() {
   const menuHamburguesas = document.getElementById("menu-hamburguesas");
+
   menuHamburguesa.forEach(hamburguesa => {
       const hamburguesaElement = document.createElement("div");
+      hamburguesaElement.classList.add("hamburguesa");
+
       hamburguesaElement.innerHTML = `
-          <h3>${hamburguesa.nombre} - $${hamburguesa.precio}
-          <p>${hamburguesa.descripcion}</p>
+          <button class="btn btn-primary agregar-al-carrito">${hamburguesa.boton}</button>
+          <div class="hamburguesa-detalles">
+              <h3>${hamburguesa.nombre} - $${hamburguesa.precio}</h3>
+              <p>${hamburguesa.descripcion}</p>
+          </div>
       `;
+
       menuHamburguesas.appendChild(hamburguesaElement);
+
+      const agregarButton = hamburguesaElement.querySelector(".agregar-al-carrito");
+      agregarButton.addEventListener('click', () => {
+          const detalles = hamburguesaElement.querySelector(".hamburguesa-detalles");
+          detalles.classList.toggle("mostrar-detalles");
+      });
   });
 }
 
-function mostrarMenu(menu) {
-  menu.forEach(hamburguesa => {
-      console.log(`${hamburguesa.nombre} - $${hamburguesa.precio}`);
-      console.log(hamburguesa.descripcion);
-  });
-}
 
-function filtrarHamburguesasPorPrecio(menu, precioMaximo) {
-    return menu.filter(hamburguesa => hamburguesa.precio <= precioMaximo);
-}
-const hamburguesasCostos = filtrarHamburguesasPorPrecio(menuHamburguesa, 10000);
-console.log(hamburguesasCostos);
 
 crearMenu();
 mostrarMenu(menuHamburguesa);
